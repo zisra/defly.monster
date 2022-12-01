fd3b66bc8a6ad953a2a9cccafbe5fef754a51d61
zisra <100528712+zisra@users.noreply.github.com>
2022-11-30 22:31:32 -0600

    -

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ api.get('/upgrades', require('./routes/upgrades.js'));
 api.post('/feedback', require('./routes/feedback.js'));
 api.get('/eliteTeams', require('./routes/eliteTeams.js'));
 api.get('/commands', require('./routes/commands.js'));
-api.get('/skin', require('./routes/skins.js'));
+api.get('/skin', require('./routes/skin.js'));
 api.get('/articles', require('./routes/articles.js'));
 
 module.exports = api;
